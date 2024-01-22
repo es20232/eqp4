08663bd9f9da5ec2cdf30a85fe149b9a52f560ab
mateusSousaMoura <mateussmoura30@gmail.com>
2024-01-22 05:36:01 -0300

    Setup REST API
    Use pip install djangorestframework to install REST
    Defined the user class
    Defined the login and signup views

--- tigrim_base/tigrim_base/urls.py
+++ tigrim_base/tigrim_base/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from .views import index
+from django.urls import path, re_path
+from . import views
+from .views import index 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    re_path('login', views.login),
+    re_path('register', views.login),
 ]
