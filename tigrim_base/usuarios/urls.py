55607436faae9dd3e1a9b9868d5bb981cf906a39
João Pedro Soares do Monte <82397148+JpSoares17@users.noreply.github.com>
2024-02-12 20:11:49 -0300

    Backend Complete

--- tigrim_base/usuarios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UsuarioList, UsuarioDetail
-
-urlpatterns = [
-    path('', UsuarioList.as_view()),
-    path('<int:pk>', UsuarioDetail.as_view()),
-]
